6713dfb4b7bc1a6729a84c049d1f9ada871d895e
Chenhao Yang <vincentyangch@gmail.com>
2026-03-17 20:40:21 -0500

    chore: add bin/start.mjs launcher with file logging for launchd

---
+++ bin/start.mjs
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import { appendFileSync } from 'fs';
+
+const LOG = '/Users/flyingchickens/Documents/Projects/CCBuddy/data/ccbuddy.log';
+const log = (msg) => appendFileSync(LOG, `[${new Date().toISOString()}] ${msg}\n`);
+
+const origLog = console.log;
+const origErr = console.error;
+console.log = (...a) => { log(a.join(' ')); origLog(...a); };
+console.error = (...a) => { log('ERR: ' + a.join(' ')); origErr(...a); };
+process.on('unhandledRejection', (err) => { log('UNHANDLED: ' + (err?.stack || err)); });
+
+log('Starting CCBuddy...');
+const { bootstrap } = await import('../packages/main/dist/bootstrap.js');
+const result = await bootstrap(
+  '/Users/flyingchickens/Documents/Projects/CCBuddy/config',
+);
+log('CCBuddy running');
+
+process.on('SIGTERM', async () => { log('SIGTERM received'); await result.stop(); process.exit(0); });
+process.on('SIGINT', async () => { log('SIGINT received'); await result.stop(); process.exit(0); });
